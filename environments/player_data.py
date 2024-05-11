333e3e0b99ca02f9fc7be368c694a2df7d33e9be
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2024-05-11 16:42:08 +0200

    Add: player representation

---
+++ environments/player_data.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+@dataclass
+class Player:
+    _value: int
+
+    def __post_init__(self):
+        if self._value != -1 and self._value != 1:
+            raise ValueError("Player value is incorrect, chose between -1 or 1.")
+
+    def __hash__(self) -> int:
+        return self._value
+
+    def __repr__(self):
+        return f"Player({self.symbol})"
+
+    @property
+    def value(self):
+        return self._value
+    
+    @property
+    def symbol(self):
+        return 'X' if self._value == -1 else 'O'
+    
+    def switch_player(self):
+        return Player(self._value * -1)
+
+    @classmethod
+    def create_x(cls):
+        return cls(-1)
+    
+    @classmethod
+    def create_y(cls):
+        return cls(1)
