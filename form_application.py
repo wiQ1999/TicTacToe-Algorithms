72253e9b5dc194b8e518545a2bd699fabe76e8f0
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2024-05-14 21:36:32 +0200

    Update: create option menu with player types

--- form_application.py
+++ form_application.py
@@ -2,18 +2,22 @@ from tkinter import *
 from tkinter import messagebox
 from environments.base_game_env import BaseGameEnv
 from environments.game_state_enum import GameState
+from players.human.human_player import HumanPlayer
+from players.random.random_player import RandomPlayer
 
 class FormApplication:
     _info_font = ('Arial', 12)
     _board_font = ('Arial', 30)
+    _players_dict = {
+        "Human": HumanPlayer(), 
+        "Random": RandomPlayer()}
 
     def __init__(self):
         self._game = BaseGameEnv()
+        self._player_x = HumanPlayer()
+        self._player_y = RandomPlayer()
         self._root = Tk()
         self._root.title("Tic Tac Toe")
-        self._player_x_label = Label()
-        self._player_y_label = Label()
-        self._status_lable = Label()
         self._create_info_section()
         self._buttons = []
         self._create_board_section()
@@ -22,28 +26,34 @@ class FormApplication:
     def _create_info_section(self):
         info = Frame(self._root)
         info.pack(side=TOP, fill=X, expand=True)
-        player_x = Frame(info)
-        player_x.grid(row=0, column=0)
-        player_x.grid_rowconfigure(0, minsize=50)
-        player_x.grid_columnconfigure(0, minsize=150)
-        Label(player_x, text="Player X", font=self._info_font).grid(row=0)
-        self._player_x_label = Label(player_x, text="Human", font=self._info_font)
-        self._player_x_label.grid(row=1)
         status = Frame(info)
+        player_x_frame = Frame(info)
+        player_x_frame.grid(row=0, column=0)
+        player_x_frame.grid_rowconfigure(0, minsize=50)
+        player_x_frame.grid_columnconfigure(0, minsize=150)
+        Label(player_x_frame, text="Player X", font=self._info_font).grid(row=0)
+        player_x_str = StringVar(player_x_frame, "Human")
+        self._player_x_cbox = OptionMenu(player_x_frame, player_x_str, 
+                                         *self._players_dict, 
+                                         command=lambda name: self._set_player(name, True))
+        self._player_x_cbox.grid(row=1)
         status.grid(row=0, column=1)
         status.grid_rowconfigure(0, minsize=50)
         status.grid_columnconfigure(0, minsize=150)
         Label(status, text="Status:", font=self._info_font).grid(row=0)
         self._status_lable = Label(status, font=self._info_font)
         self._status_lable.grid(row=1)
-        Button(status, text='Resetuj', font=self._info_font, command=lambda: self.restart_game()).grid(row=1)
-        player_y = Frame(info)
-        player_y.grid(row=0, column=2)
-        player_y.grid_rowconfigure(0, minsize=50)
-        player_y.grid_columnconfigure(0, minsize=150)
-        Label(player_y, text="Player O", font=self._info_font).grid(row=0)
-        self._player_y_label = Label(player_y, text="Human", font=self._info_font)
-        self._player_y_label.grid(row=1)
+        Button(status, text='Resetuj', font=self._info_font, command=self.restart_game).grid(row=1)
+        player_y_frame = Frame(info)
+        player_y_frame.grid(row=0, column=2)
+        player_y_frame.grid_rowconfigure(0, minsize=50)
+        player_y_frame.grid_columnconfigure(0, minsize=150)
+        Label(player_y_frame, text="Player O", font=self._info_font).grid(row=0)
+        player_y_str = StringVar(player_y_frame, "Random")
+        self._player_y_cbox = OptionMenu(player_y_frame, player_y_str, 
+                                         *self._players_dict, 
+                                         command=lambda name: self._set_player(name, False))
+        self._player_y_cbox.grid(row=1)
 
     def _create_board_section(self):
         board = Frame(self._root)
@@ -51,7 +61,9 @@ class FormApplication:
         for x in range(3):
             row = []
             for y in range(3):
-                pos = Button(board, text='', font=self._board_font, command=lambda ix=x, iy=y: self._set_position(ix, iy))
+                pos = Button(board, text='', 
+                             font=self._board_font, 
+                             command=lambda ix=x, iy=y: self._set_position(ix, iy))
                 pos.grid(row=x, column=y, sticky="nsew")
                 board.grid_columnconfigure(x, minsize=150)
                 board.grid_rowconfigure(y, minsize=150)
@@ -63,6 +75,17 @@ class FormApplication:
         for x in range(3):
             for y in range(3):
                 self._buttons[x][y].config(state=button_state)
+
+    def _set_player(self, name: StringVar, is_x: bool):
+        new_player = self._players_dict[name]
+        new_player_type = type(new_player)
+        if is_x and type(self._player_x) is not new_player_type:
+            self._player_x = new_player
+        elif not is_x and type(self._player_y) is not new_player_type:
+            self._player_y = new_player
+        else: 
+            return
+        self.restart_game()
 
     def _set_position(self, x: int, y: int):
         if self._game.is_position_taken(x, y):
