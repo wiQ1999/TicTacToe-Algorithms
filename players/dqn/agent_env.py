3d7bfc4f1335ad5c126e729ce7c396b806e9c86a
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2024-06-21 23:59:34 +0200

    Feat: change network parameters

--- players/dqn/agent_env.py
+++ players/dqn/agent_env.py
@@ -4,33 +4,41 @@ from environments.base_game_env import BaseGameEnv
 from environments.game_state_enum import GameState
 
 class AgentEnv:
-    def __init__(self, game: BaseGameEnv):
-        self.game = game
-
+    def __init__(self):
+        self._game = BaseGameEnv()
+        self._wrong_move = False
+    
     def reset(self) -> np.ndarray:
-        self.game.restart()
+        self._game.restart()
+        self._wrong_move = False
         return self.get_state()
     
     def available_actions(self) -> List[Tuple[int, int]]:
-        return self.game.available_moves()
+        return self._game.available_moves()
 
     def get_state(self) -> np.ndarray:
-        return np.array(self.game._board).flatten()
+        return np.array(self._game._board).flatten()
 
     def get_reward(self) -> Literal[-1, 0, 1]:
-        if self.game.game_state == GameState.X_WIN:
-            return 1 if self.game.current_player.is_o else -1
-        elif self.game.game_state == GameState.O_WIN:
-            return 1 if self.game.current_player.is_x else -1
+        if self._wrong_move:
+            return -1
+        elif self._game.game_state == GameState.X_WIN:
+            return 1 if self._game.current_player.is_o else -1
+        elif self._game.game_state == GameState.O_WIN:
+            return 1 if self._game.current_player.is_x else -1
         else:
             return 0
 
     def is_done(self):
-        return self.game.game_state != GameState.PLAYING
+        return self._wrong_move or self._game.game_state != GameState.PLAYING
     
-    def step(self, action: Tuple[int, int]) -> Tuple[np.ndarray, Literal[-1, 0, 1], bool]:
-        x, y = action
-        self.game.move(x, y)
+    def step(self, action: int) -> Tuple[np.ndarray, Literal[-1, 0, 1], bool]:
+        x = action // 3
+        y = action % 3
+        if self._game.is_position_taken(x, y):
+            self._wrong_move = True
+        else:
+            self._game.move(x, y)
         state = self.get_state()
         reward = self.get_reward()
         done = self.is_done()
--- players/dqn/dq_network.py
+++ players/dqn/dq_network.py
@@ -1,19 +1,15 @@
-import torch
 import torch.nn as nn
 
 class DQNetwork(nn.Module):
     def __init__(self):
         super(DQNetwork, self).__init__()
-        self.flatten = nn.Flatten(0, 1)
         self.seq = nn.Sequential(
-            nn.Linear(9, 36),
+            nn.Linear(9, 32),
             nn.ReLU(),
-            nn.Linear(36, 36),
+            nn.Linear(32, 32),
             nn.ReLU(),
-            nn.Linear(36, 9),
-            nn.Sigmoid()
+            nn.Linear(32, 9)
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         return self.seq(x)
